c74d17363829ee06a76e6e696ac53f7175993c20
zhan-fg <zhanqiuhui@live.com>
2018-07-03 17:09:09 +0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,17 +7,3 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-'''from . import models
-def index(request):
-    articles = models.Article.objects.all()
-    return render(request, 'index.html', {'articles':articles})
-
-
-def article_page(request, article_id):
-    article = models.Article.objects.get(pk=article_id)
-    return  render(request, '', {'article':article})
-
-
-def edit_page(request):
-    return  render(request, 'edit_page.html')
-'''
